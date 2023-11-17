90c5106d6f5e9b149a0161af553591bc5e653020
fazaaITRepo <git@fazaa.ae>
2023-11-17 11:33:15 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import bgGif from "../src/assets/images/bg-gif.gif";
 function App() {
   return (
     <Container maxWidth="xl">
-      <Grid container>
+      <Grid container sx={{ minHeight: "100vh" }}>
         <Grid item xs={4} md={2}>
           <Box>
             <img width="100%" src={logo} alt="logo" />
@@ -19,14 +19,27 @@ function App() {
             display: "flex",
             alignItems: "center",
             flexDirection: "column",
+            "& img": {
+              width: { xs: "100%", md: "50%" },
+            },
           }}
         >
-          <img width="50%" src={bgGif} alt="" />
-          <Typography variant="h4" color="#204c73" gutterBottom>
+          <img src={bgGif} alt="working on it" />
+          <Typography
+            variant="h4"
+            color="#204c73"
+            gutterBottom
+            sx={{ textAlign: "center" }}
+          >
             Get ready for the change
           </Typography>
-          <Typography variant="body1" color="#204c73">
-            We are working on something amazing. Please check back later to know more !!
+          <Typography
+            variant="body1"
+            color="#204c73"
+            sx={{ textAlign: "center" }}
+          >
+            We are working on something amazing. Please check back later to know
+            more !!
           </Typography>
         </Grid>
       </Grid>
