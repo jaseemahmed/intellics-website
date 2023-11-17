09f547c9b7f3a60420fbc27e634e7fbf6a8bcb72
fazaaITRepo <git@fazaa.ae>
2023-11-17 11:14:20 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { Box, Container, Grid, Typography } from "@mui/material";
+import React from "react";
+import logo from "../src/assets/images/logo.jpg";
+import bgGif from "../src/assets/images/bg-gif.gif";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <Container maxWidth="xl">
+      <Grid container>
+        <Grid item xs={4} md={2}>
+          <Box>
+            <img width="100%" src={logo} alt="logo" />
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+          }}
+        >
+          <img width="50%" src={bgGif} alt="" />
+          <Typography variant="h4" color="#204c73" gutterBottom>
+            Get ready for the change
+          </Typography>
+          <Typography variant="body1" color="#204c73">
+            We are working on something amazing. Please check back later to know more !!
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
