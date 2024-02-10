747c30c512b8605008b34e29fe0539b6461071b2
fazaaITRepo <git@fazaa.ae>
2024-02-10 13:34:47 +0400

    partners added and applied slider

--- src/features/Partners/Partners.js
+++ src/features/Partners/Partners.js
@@ -1,17 +1,150 @@
-import { Grid } from "@mui/material";
+import { Box, Card, CardMedia, Grid, useTheme } from "@mui/material";
 import React from "react";
-import p1 from "../../assets/images/p1.jpg";
-import p2 from "../../assets/images/p2.jpg";
-import p3 from "../../assets/images/p3.jpg";
-import p5 from "../../assets/images/p5.jpg";
-import p6 from "../../assets/images/p6.jpg";
-import p7 from "../../assets/images/p7.jpg";
+import symantec from "../../assets/images/symantec.jpg";
+import eset from "../../assets/images/eset.jpg";
+import fortinet from "../../assets/images/fortinet.jpg";
+import microsoft from "../../assets/images/microsoft.jpg";
+import sophos from "../../assets/images/sophos.jpg";
+import hemidal from "../../assets/images/hemidal.jpg";
+import veeam from "../../assets/images/veeam.jpg";
+import watchguard from "../../assets/images/watchguard.jpg";
+import lenovo from "../../assets/images/lenovo.jpg";
+import dell from "../../assets/images/dell.jpg";
+import veritas from "../../assets/images/veritas.jpg";
+import kaspersky from "../../assets/images/kaspersky.jpg";
+import zebra from "../../assets/images/zebra.jpg";
 import { motion } from "framer-motion";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-const Partners = () => {
+const Partners = ({ isMobile }) => {
+  const theme = useTheme();
+  const settings = {
+    dots: false,
+    infinite: true,
+    centerMode: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 3,
+    easing: "ease-in-out",
+    arrows: false,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+    ],
+  };
+  const partners = [
+    {
+      title: "symantec",
+      image: symantec,
+    },
+    {
+      title: "eset",
+      image: eset,
+    },
+    {
+      title: "fortinet",
+      image: fortinet,
+    },
+    {
+      title: "microsoft",
+      image: microsoft,
+    },
+    {
+      title: "sophos",
+      image: sophos,
+    },
+    {
+      title: "hemidal",
+      image: hemidal,
+    },
+    {
+      title: "veeam",
+      image: veeam,
+    },
+    {
+      title: "watchguard",
+      image: watchguard,
+    },
+    {
+      title: "lenovo",
+      image: lenovo,
+    },
+    {
+      title: "dell",
+      image: dell,
+    },
+    {
+      title: "veritas",
+      image: veritas,
+    },
+    {
+      title: "kaspersky",
+      image: kaspersky,
+    },
+    {
+      title: "zebra",
+      image: zebra,
+    },
+  ];
   return (
-    <Grid container sx={{ "& img": { filter: "grayscale(1)" } }}>
-      <Grid
+    <Grid
+      container
+      spacing={2}
+      sx={{ "& img": { filter: isMobile ? "grayscale(0)" : "grayscale(1)" } }}
+    >
+      <Grid item xs={12}>
+        <Slider {...settings}>
+          {partners.map((item, index) => (
+            // <Card sx={{ position: "relative", display: "flex" }} key={index} elevation={0}>
+            //   <CardMedia
+            //     alt={item.title}
+            //     component="img"
+            //     image={item.image}
+            //     sx={{
+            //       height: "200px",
+            //       objectFit: "contain",
+            //       // width: "100%",
+            //     }}
+            //     // className={classes.carousalMedia}
+            //   />
+            // </Card>
+            <Box sx={{ padding: isMobile ? "1rem" : '3rem' }}>
+              <motion.img
+                whileHover={{
+                  filter: "grayscale(0)",
+                  scale: "1.1",
+                  transition: {
+                    duration: 0.25,
+                    type: "spring",
+                    stiffness: 100,
+                    ease: "linear",
+                  },
+                }}
+                width="100%"
+                src={item.image}
+                alt="microsoft partner"
+              />
+            </Box>
+          ))}
+        </Slider>
+      </Grid>
+      {/* <Grid
         item
         xs={6}
         md={2}
@@ -112,7 +245,7 @@ const Partners = () => {
           src={p7}
           alt="microsoft partner"
         />
-      </Grid>
+      </Grid> */}
     </Grid>
   );
 };
